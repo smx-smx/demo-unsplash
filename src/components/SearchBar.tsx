8e2a2bd4d614617eea468d7ab0b4407dbb58f758
Stefano Moioli <smxdev4@gmail.com>
2024-03-24 16:34:53 +0100

    implement random search for initial load/empty query

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,29 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'flowbite-react';
 
 interface Props {
+  initialQuery: string;
   onSearch: (query: string) => void;
 }
 
-function SearchBar({ onSearch }: Props): JSX.Element {
+function SearchBar({ initialQuery, onSearch }: Props): JSX.Element {
   const [query, setQuery] = useState<string>('');
+  const [dirty, setDirty] = useState<boolean>(false);
 
   const handleSearch = (): void => {
     onSearch(query);
   };
 
+  const getQuery = () => {
+    // use user query, if modified
+    if (query.length > 0 || dirty) return query;
+    return initialQuery;
+  };
+
+  // reset keyword if prop changed
+  useEffect(() => {
+    setDirty(false);
+  }, [initialQuery]);
+
   return (
     <div className='flex flex-row'>
       <input
-			type="text"
-			placeholder='Search for pictures...'
-			value={query}
-			onChange={(e) => setQuery(e.target.value)} 
-			onKeyDown={(e) => {
-				if(e.key == 'Enter') handleSearch();
-			}}
-		/>
-      	<Button onClick={handleSearch}>Search</Button>
+        type="text"
+        placeholder='Search for pictures...'
+        value={getQuery()}
+        onChange={(e) => {
+          setDirty(true);
+          setQuery(e.target.value)
+        }}
+        onKeyDown={(e) => {
+          if (e.key == 'Enter') handleSearch();
+        }}
+      />
+      <Button onClick={handleSearch}>Search</Button>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { HeadFC, PageProps } from "gatsby";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createApi } from 'unsplash-js';
 import * as PhotoApi from 'unsplash-js/dist/methods/photos/types';
 import SearchBar from "../components/SearchBar";
@@ -7,13 +7,29 @@ import ImageList from "../components/ImageList";
 import { Spinner } from "flowbite-react";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const [query, setQuery] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const [images, setImages] = useState<PhotoApi.Basic[]>([]);
-  
+
   const api = createApi({
     accessKey: window.localStorage.getItem('unsplash.apikey') ?? ''
   });
-  
+
+  const getRandomKeyword = () => {
+    const keywords = [
+      'Nature', 'Landscape', 'Animals', 'City', 'Food',
+      'Travel', 'Art', 'Architecture', 'Technology', 'Music'
+    ];
+    const randomIndex = Math.floor(Math.random() * keywords.length);
+    return keywords[randomIndex];
+  };
+
+  const randomSearch = () => {
+    const randomQuery = getRandomKeyword();
+    setQuery(randomQuery);
+    fetchImages(randomQuery);
+  };
+
   const fetchImages = async (query: string) => {
     setLoading(true);
     const resp = await api.search.getPhotos({
@@ -24,11 +40,23 @@ const IndexPage: React.FC<PageProps> = () => {
     setLoading(false);
   };
 
+  useEffect(() => {
+    randomSearch();
+  }, []);
+
   return (
     <main className="dark:bg-gray-800">
       <div>
         <div className="flex flex-row">
-          <SearchBar onSearch={fetchImages} />
+          <SearchBar initialQuery={query}
+            onSearch={query => {
+              if (query.length < 1) {
+                randomSearch();
+              } else {
+                setQuery(query);
+                fetchImages(query);
+              }
+            }} />
           {loading && <Spinner size={"xl"} />}
         </div>
         <ImageList images={images} />

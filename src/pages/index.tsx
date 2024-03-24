c6ea72bbd801a17829533ecd2d3620c0866d44fe
Stefano Moioli <smxdev4@gmail.com>
2024-03-24 18:24:36 +0100

    add navbar and user menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import { createApi } from 'unsplash-js';
 import * as PhotoApi from 'unsplash-js/dist/methods/photos/types';
 import SearchBar from "../components/SearchBar";
 import ImageList from "../components/ImageList";
-import { Spinner } from "flowbite-react";
+import { Dropdown, Spinner } from "flowbite-react";
+import { Navbar } from "flowbite-react";
+import { Link } from "gatsby"
 
 const IndexPage: React.FC<PageProps> = () => {
   const [query, setQuery] = useState<string>('');
@@ -46,6 +48,30 @@ const IndexPage: React.FC<PageProps> = () => {
 
   return (
     <main className="dark:bg-gray-800">
+      <Navbar fluid rounded>
+        <Navbar.Brand>
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Unsplash Demo</span>
+        </Navbar.Brand>
+        <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <div className="relative size-10 overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
+                <svg className="absolute -left-1 size-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path></svg>
+            </div>
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm text-gray-400">User Menu</span>
+          </Dropdown.Header>
+          <Dropdown.Item>
+            <Link to="/favourites">Favourites</Link>
+          </Dropdown.Item>
+        </Dropdown>
+        <Navbar.Toggle />
+      </div>
+      </Navbar>
       <div>
         <div className="m-3 flex flex-row justify-center">
           <SearchBar initialQuery={query}

e54c4a2fe160656d768ec0e4d0c5bb83fea0d4fb
Stefano Moioli <smxdev4@gmail.com>
2024-03-24 17:05:38 +0100

    implement picture favourite marking

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card } from 'flowbite-react';
+import React, { useEffect, useState } from 'react';
+import { Card, Rating } from 'flowbite-react';
 import * as PhotoApi from 'unsplash-js/dist/methods/photos/types';
 
 interface ImageProps {
@@ -7,15 +7,57 @@ interface ImageProps {
 }
 
 function Image({ data }: ImageProps) {
+  const KEY_FAVOURITES = 'unsplash.favourites';
+
+  const getFavourites = () => {
+    try {
+      return JSON.parse(window.localStorage.getItem(KEY_FAVOURITES) ?? '{}');
+    } catch(e){
+      return {};
+    }
+  }
+
+  const isImageFavourite = (id:string) => {
+    return getFavourites()[id] !== undefined;
+  }
+
+  const [isFavourite, setFavourite] = useState<boolean>(false);
+
+  const toggleFavourite = (id:string) => {
+    const favourites = getFavourites();
+    if(id in favourites){
+      delete favourites[id];
+      setFavourite(false);
+    } else {
+      favourites[id] = true;
+      setFavourite(true);
+    }
+    window.localStorage.setItem(KEY_FAVOURITES, JSON.stringify(favourites));
+  };
+
+  useEffect(() => {
+    setFavourite(isImageFavourite(data.id));
+  }, []);
+
   return <>
     <Card
       className="max-w-sm"
       imgAlt={data.alt_description ?? ''}
       imgSrc={data.urls.thumb}
     >
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        {data.description}
-      </p>
+      <div className='relative'>
+        <div className='absolute right-0 top-0' style={{
+          marginTop: '-20px',
+          marginRight: '-20px'
+        }}>
+        <Rating size="md">
+          <Rating.Star onClick={() => toggleFavourite(data.id)} filled={isFavourite} />
+        </Rating>
+        </div>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {data.description}
+        </p>
+      </div>
     </Card>
   </>;
 }

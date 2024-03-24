b583e90481a7f75e46814c58a745db0aa02cfab5
Stefano Moioli <smxdev4@gmail.com>
2024-03-24 16:40:12 +0100

    fix formatting

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,21 +3,21 @@ import { Card } from 'flowbite-react';
 import * as PhotoApi from 'unsplash-js/dist/methods/photos/types';
 
 interface ImageProps {
-	data: PhotoApi.Basic;
+  data: PhotoApi.Basic;
 }
 
-function Image({ data } : ImageProps ){
-	return <>
-		<Card
-			className="max-w-sm"
-			imgAlt={data.alt_description ?? ''}
-			imgSrc={data.urls.thumb}
-		>
-			<p className="font-normal text-gray-700 dark:text-gray-400">
-				{data.description}
-			</p>
-		</Card>
-	</>;
+function Image({ data }: ImageProps) {
+  return <>
+    <Card
+      className="max-w-sm"
+      imgAlt={data.alt_description ?? ''}
+      imgSrc={data.urls.thumb}
+    >
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {data.description}
+      </p>
+    </Card>
+  </>;
 }
 
 export default Image;
--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -3,14 +3,14 @@ import * as PhotoApi from 'unsplash-js/dist/methods/photos/types';
 import Image from './Image';
 
 interface ImageListProps {
-	images: PhotoApi.Basic[];
+  images: PhotoApi.Basic[];
 }
 
 function ImageList({ images }: ImageListProps) {
   return (
     <div className='flex flex-wrap items-start gap-3'>
       {images.map((image) => (
-		<Image key={image.id} data={image} />
+        <Image key={image.id} data={image} />
       ))}
     </div>
   );

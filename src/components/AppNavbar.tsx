eb0ed72b0b29628da1ee229ff9c8b010e6fe0bbe
Stefano Moioli <smxdev4@gmail.com>
2024-03-25 01:44:44 +0100

    navbar: add home link (click on title) and remove toggle (for mobile devices)

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 import { Dropdown, Navbar } from "flowbite-react";
-import { Link } from "gatsby"
+import { Link } from "gatsby";
 
-function AppNavbar(){
+function AppNavbar() {
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand>
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Unsplash Demo</span>
+      <Navbar.Brand as={Link} href="/">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Unsplash Demo
+        </span>
       </Navbar.Brand>
       <div className="flex md:order-2">
         <Dropdown
@@ -14,7 +16,17 @@ function AppNavbar(){
           inline
           label={
             <div className="relative size-10 overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600">
-                <svg className="absolute -left-1 size-12 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"></path></svg>
+              <svg
+                className="absolute -left-1 size-12 text-gray-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                ></path>
+              </svg>
             </div>
           }
         >
@@ -25,7 +37,6 @@ function AppNavbar(){
             <Link to="/favourites">Favourites</Link>
           </Dropdown.Item>
         </Dropdown>
-        <Navbar.Toggle />
       </div>
     </Navbar>
   );
